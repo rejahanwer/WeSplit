0a4b4e8b1560bf5ccc324d361af15a9e204abbbf
Rejah Anuvar <rejahanwer@gmail.com>
2021-05-12 12:58:54 +0530

    Split complete.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var noOfPeople = ""
+    @State private var tipPercentage = 0
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalAmount : Double {
+        guard let checkAmount = Double(self.checkAmount) else {
+            return 0.0
+        }
+        
+        let total = checkAmount + (checkAmount * Double(tipPercentages[self.tipPercentage]) / 100 )
+        
+        return total
+    }
+    
+    var splitAmount: Double {
+        guard let numPeople = Double(self.noOfPeople),
+              numPeople > 0 else { return 0 }
+        
+        return self.totalAmount / numPeople
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount: ", text: self.$checkAmount).keyboardType(.decimalPad)
+
+                    TextField("No of People: ", text: self.$noOfPeople).keyboardType(.numberPad)
+                }
+
+                Section(header: Text("How much tip percentage?")) {
+                    Picker("TipPercentage: ", selection: self.$tipPercentage) {
+                        ForEach(0 ..< self.tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0]) %")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+
+                Section(header: Text("Total Amount")) {
+                    Text("\(self.totalAmount, specifier: "%.2f")")
+                }
+
+                Section(header: Text("Amount per person")) {
+                    Text("\(self.splitAmount , specifier: "%.2f")")
+                }
+            }.navigationBarTitle("WeSplit")
+        }
     }
 }
 
