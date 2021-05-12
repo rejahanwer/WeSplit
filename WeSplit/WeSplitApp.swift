d0acd6657065697d104c8dc55dcd3cd42f056921
Rejah Anuvar <rejahanwer@gmail.com>
2021-05-12 10:43:18 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rejah Anuvar on 12/05/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
